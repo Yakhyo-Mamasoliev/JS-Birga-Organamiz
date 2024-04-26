5dfaa80b6dfa9b09f59b823c303ae5d279091bf1
Yakhyo-Mamasoliev <166381671+Yakhyo-Mamasoliev@users.noreply.github.com>
2024-04-26 23:53:11 +0900

    Update 04_classlar.js

--- 04_classlar.js
+++ 04_classlar.js
@@ -1,4 +1,4 @@
-// // 04_CLASSES bu juda ko`p Objectlarni qisqa kod bilan hosil qilish uchun
+// // 04_CLASSES(this is a template) bu juda ko`p Objectlarni qisqa kod bilan hosil qilish uchun
 
 // //naming standards: Camel, Snake, Kebab, Pascal case
 // //camel case
@@ -10,44 +10,44 @@
 // //kebab case-asosan filelarni nomlashga ishlatiladi
 // //example is: my-file.ts
 
-// //Pascal case: Asosan classlarda ishlatiladi:   class => PersonGroup
+// //Pascal case: Asosan classlarda ishlatiladi:   class => PersonGroup,  (or Person)
 
 
 
 
-//CLASS tashkiliy jihatdan Person, Constructor, Method, 
-class Person{
-  //State
-  name = "";
-  age = 0;
+// //CLASS tashkiliy jihatdan State, Constructor, Method, 
+// class Person{
+//   //State
+//   name = "";
+//   age = 0;
 
-  //Constructor
-  constructor(personName, personAge) {
-    this.name = personName;
-    this.age = personAge;
-  }
+//   //Constructor
+//   constructor(personName, personAge) {
+//     this.name = personName;
+//     this.age = personAge;
+//   }
 
-  //Medthod
-  introduce(){
-    console.log(`My name is , ${this.name} and I am ${this.age} years old`);
-  }
+//   //Medthod
+//   introduce(){
+//     console.log(`My name is , ${this.name} and I am ${this.age} years old`);
+//   }
 
-  greet(){
-    console.log('Hi, How are you? ');
-  }
-  static help(){
-    console.log('You need help bro?!');
-  }
-}
+//   greet(){
+//     console.log('Hi, How are you? ');
+//   }
+//   static help(){
+//     console.log('You need help bro?!');
+//   }
+// }
 
-person1 = new Person("MArtin", 33);
-person2 = new Person("Mike", 30);
-person3 = new Person("SAmuel", 20);
+// person1 = new Person("Martin", 33);
+// person2 = new Person("Mike", 30);
+// person3 = new Person("SAmuel", 20);
 
-person1.greet();
-person1.introduce();
+// person1.greet();
+// person1.introduce();
 
 
 
-//Static MEthod <==> Class /// static medthod or properties is not same as class objects meaning, objects are not required for static to be exist, 
-Person.help();
+// //Static MEthod <==> Class /// static medthod or properties is not same as class objects meaning, objects are not required for static to be exist, 
+// Person.help();
