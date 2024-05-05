9d8f81c80ef730150ad166a570a92b785965766f
Yakhyo-Mamasoliev <165603169+Yakhyo-Mamasoliev@users.noreply.github.com>
2024-05-06 02:38:18 +0900

    .

---
+++ 12 NodeJS tanlovi.js
@@ -0,0 +1,15 @@
+// WEB(html, css, js) 
+
+// JSni webdan tahsqarida ishlatish uchun NODEJS yaratilgan
+// V8 va libuv degan enjinlar yordamida yaratilgan (both engines created using C++)
+
+// Real time is the most important in Today`s webpages
+
+// NESTJS is build via ExpresJS so learn expressJs first
+
+// nvm nodeni turli xil versialarni install qilish uchun kerak
+// cmd commands
+// nvm --version => checks if nvm is installed before
+// nvm list => shows installed nvm versions
+//  nvm install 14.7.0 => installs exact 14.7.0 version
+// nvm uninstall 14.7.0 => ininstalls
---
+++ 13-14 Nodejs va git install qilamiz.js
@@ -0,0 +1,20 @@
+// Client va Server
+
+// 2 xil usul 
+// 1. Backend Server Side Rendering yordamida serverda html qurish(cleintga yuborish)
+// bu usulda serverga kirgan inson htmlni darxol oladi
+
+// 2. 
+// JSON data type yordamida browserga yuboramiz
+// Tog`rirog`i Reactga yuboramiz, React o`zi undan html qurib oladi
+
+
+
+// KERNEL - applicationlar va cpu, memory, other hardware devicelarni bir biriga bog`lab beradigan markaziy tizim
+
+// Applications <=> KERNEL <=> Cpu&Memomory&Devices
+
+// Linux yordmada macOS va WindowsOSlar qurilgan 
+// macOS kerneli Linux terminaliga o`xshash ishlashi va bir xil kod bilan ishlashligi uchun u yaxshiroq!
+
+// We can write codes on Linux and macOS kernel but not on Windows comment line that is why we use PowerShell.
