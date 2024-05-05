72b93af7f95ad663e93ee74acb0e5c82b0d1fb09
Yakhyo-Mamasoliev <166381671+Yakhyo-Mamasoliev@users.noreply.github.com>
2024-05-05 23:45:18 +0900

    Add files via upload

---
+++ 10_Looplarni organamiz.js
@@ -0,0 +1,37 @@
+// // while and loops
+
+
+// // while => when we do not know number of iterations (when user enters valid username till proper one)
+// let count = 0;
+// while(count <= 10) {
+//   console.log('executed:', count);
+//   count += 1;
+// }
+
+
+// // for => when we know number of iterations (items in basket of array)
+// for (let i = 0; i <= 10; i++) {
+//   console.log('executed:', i);
+// }
+
+
+// // iteration methods: map, filter, ...
+// const fruits = ['apple', 'fruit', 'cherry'];
+// fruits.map((ele, index) => {
+//     console.log(`${index}: ${ele}`)
+// });
+
+// // of it gives us values
+// // in it gives us indexes
+// // printing items through loop
+
+
+// // of 
+// for(const ele of fruits) {
+//   console.log(ele);
+// }
+
+// // in
+// for(const index in fruits) {
+//   console.log(index);
+// }
